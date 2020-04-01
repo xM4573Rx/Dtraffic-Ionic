7f919bd436b398e288457486f8615f90916626cb
JORGE <jorgealopez@unicesar.edu.co>
2020-04-01 01:57:57 -0500

    1.0.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var WifiWizard2: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  results = [];
+  infoText = '';
 
+  constructor() {
+    this.getSSID();
+  }
+
+  async getSSID() {
+    this.infoText = 'Loading...';
+    try {
+      const results = await WifiWizard2.scan();
+      this.results = results;
+      this.infoText = '';
+    } catch (error) {
+      this.infoText = error;
+    }
+    console.log(this.results);
+  }
 }
